25b7aec84d04c2be3d978e3305d15a8d915e19ee
adeshkumar1 <89107712+adeshkumar1@users.noreply.github.com>
2022-12-15 11:31:44 -0600

    asdf

---
+++ Day7/d7.java
@@ -0,0 +1,12 @@
+package Day7;
+import java.util.*;
+import java.io.*;
+
+public class d7 
+{
+    public static void main (String [] args) throws IOException
+    {
+        File input = new File("/Users/adesh/Documents/GitHub/AdventOfCode/Day6/aaa.txt");
+        Scanner scan = new Scanner(input);
+    }
+}
